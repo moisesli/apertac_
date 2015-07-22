c5ca494386c8d9dad59a19fb8965666a00d603e7
moises <moises@Mac-Pro-de-moises.local>
2015-07-22 09:11:33 -0700

    moises

--- app/controllers/inicios_controller.rb
+++ app/controllers/inicios_controller.rb
@@ -1,11 +1,11 @@
 class IniciosController < ApplicationController
   def index  	
-  	@presidente = Presindente.last
+  	@presidentes = Presindente.limit(5)
   	@documentos = Documento.limit(5)
   	@anuncios = Anuncio.limit(5)
   	@paginas = ApertacPagina.limit(5)
   	@directorios = Directorio.limit(5)
   	@webamigas = Webamiga.limit(5)
-  	@noticias = Noticia.limit(5)
+  	@noticias = Noticia.limit(3)
   end
 end
